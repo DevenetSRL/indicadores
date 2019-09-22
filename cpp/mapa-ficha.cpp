f2cd7c91009299efb1ea5e44431569ed99155ed4
Alejandro Salamanca <alejandro@apolo.localhost>
2019-09-22 12:52:22 -0400

    Documentación y licencia

--- cpp/mapa-ficha.cpp
+++ cpp/mapa-ficha.cpp
@@ -1,3 +1,36 @@
+/**
+ * @file mapa-ficha.cpp
+ *
+ * @brief Webservice que construye un mapa departamental iluminando un
+ * municipio en particular.
+ *
+ * @ingroup Middleware
+ *
+ * @author Alejandro Salamanca <alejandro@devenet.net>
+ * @author Virginia Kama <virginia@devenet.net>
+ * @author Josué Gutiérrez Quino <jquino@devenet.net>
+ * @author Javier Ramiro Castillo Tarqui <rcastillo@devenet.net>
+ *
+ */
+
+/* 
+ * This file is part of the indicadores-municipales distribution
+ * (https://fam.egob.org or
+ * https://github.com/DevenetSRL/indicadores).
+ * Copyright (c) 2019 Devenet SRL.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <fstream>
 #include <cgicc/Cgicc.h>
 #include <cgicc/HTTPContentHeader.h>
